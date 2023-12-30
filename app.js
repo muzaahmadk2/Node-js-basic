e37f59e0a3d2d0b62cbafd9389c3511dfd699ac4
muzaahmadk2 <muzaahmadk2@gmail.com>
2023-12-30 16:12:06 +0530

    custome response

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const http =require('http');
+const http = require("http");
 
-const server = http.createServer((req,res)=>{
-
-    console.log(req.url,req.method);
-    res.write('<h1>Muzammil</h1>');
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/home") {
+    res.write("<h1>Welcome to home page</h1>");
     res.end();
-})
+  } else if (url === "/about") {
+    res.write("<h1>Welcome to about pahe</h1>");
+    res.end();
+  } else if (url === "/node") {
+    res.write("<h1>Welcome to node page</h1>");
+    res.end();
+  } else {
+    res.write("<h1>Welcome to my page</h1>");
+    res.end();
+  }
+});
 
 server.listen(4000);
