7b3f6b49d83421fb88196225652571342e7c2b94
muzaahmadk2 <muzaahmadk2@gmail.com>
2024-01-02 17:53:19 +0530

    introt o middleware routes

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
 app.use((req,res,next)=>{
-console.log('in the middleware');
-next();
-})
-app.use((req,res,next)=>{
-    console.log('in the another middleware')
-    res.send('<h1>hello from express</h1>')
+    res.status(404).send('<h1>Page not found</h1>')
 })
 
 app.listen(4000);
