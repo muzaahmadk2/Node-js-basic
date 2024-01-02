6007c4803f0d097d49cb4546be518de3c2cd480a
muzaahmadk2 <muzaahmadk2@gmail.com>
2024-01-02 21:09:04 +0530

    Serving HTML , CSS and JS (VIEWS)

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>'
-  );
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,36 +1,25 @@
 const express = require("express");
+const path = require('path');
 const fs = require("fs");
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send(
-    `<p>${fs.readFileSync(
-      "message.txt",
-      "utf8"
-    )}<form action="/" method="POST" onSubmit="document.getElementById('username').value=localStorage.getItem('username')"><input type="text" name="message"/><input type="hidden" id="username" name="username"><button type="submit">Send</button>`
-  );
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 });
 
-router.post("/", (req, res, next) => {
+
+router.get("/contactus", (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'contact.html'))
+
+});
+
+router.post("/success", (req, res, next) => {
   console.log(req.body);
-  const data = `${req.body.username} : ${req.body.message}`;
-  fs.appendFile("message.txt", data + "\n", "utf8", (err) => {
-    console.log("Data appended to file successfully");
-    res.redirect("/");
-  });
-});
+  res.sendFile(path.join(rootDir, 'views', 'success.html'))
 
-router.get("/login", (req, res, next) => {
-  res.send(
-    '<form onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/user" method="POST"><input id="username" type="text" name="username"><br><button type="submit">Login</button></form> '
-  );
-});
-
-router.post("/user", (req, res, next) => {
-  console.log(req.body);
-
-  res.redirect("/");
 });
 
 module.exports = router;
